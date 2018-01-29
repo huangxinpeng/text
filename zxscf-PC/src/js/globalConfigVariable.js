e9867a2629dcf3995056ed203ab1b8830ee82a94
ZTE-WX <767501635@qq.com>
2018-01-29 17:01:24 +0800

    1

--- zxscf-PC/src/js/globalConfigVariable.js
+++
@@ -1,8 +0,0 @@
-/**
- * 全局可配置变量
- */
-(function($w) {
-
-    //$w.RGGeneratePowerAttorneyID = ;
-
-}(window));
--- zxscf-PC/src/wx/js/biz/public.js
+++
@@ -1,9 +0,0 @@
-var public={
-  openId:function(){
-    //return openId='012';
-    return openId=getParams().openId;
-  }
-};
-//引用公用
-var openId=public.openId();
-//console.log(openId);
